7b11d5bde094a97a185e8f1661a315494d7afea6
YatiZ <zuzu62113@gmail.com>
2023-10-03 13:04:34 +0630

    firstEditingDesign

--- src/components/Body/BodyPage.jsx
+++ src/components/Body/BodyPage.jsx
@@ -2,14 +2,14 @@ import FooterPage from "../Footer/FooterPage";
 import Content from "./Content";
 import MainImg from "./MainImg";
 import SliderPage from "./SliderPage";
-import User from "./User";
+// import User from "./User";
 
 function BodyPage(){
     return <div className="">
     
     <Content />
     {/* <MainImg/> */}
-    <User/>
+    {/* <User/> */}
     <SliderPage />
     <FooterPage />
     </div>
