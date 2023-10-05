606062999aaf909226bd77aed309691b5ede752e
YatiZ <zuzu62113@gmail.com>
2023-10-05 16:46:24 +0630

    adjust panel

--- src/components/Body/Shoes/ShoeData.jsx
+++ src/components/Body/Shoes/ShoeData.jsx
@@ -4,14 +4,15 @@ import React, { useEffect, useState } from "react";
 const ShoeData = ({ shoe }) => {
   const renderedItems = shoe.data.map((d, index) => {
     return (
-      <Panel key={index}>
+      <Panel key={index} className='w-fit'>
         <p>{d.name}</p>
         <img className="w-60" src={d.img} alt="img" />
       </Panel>
     );
   });
 
-  return <div>{renderedItems}</div>;
+  return <div className="grid grid-cols-3 gap-4 px-10">
+    {renderedItems}</div>;
 };
 
 export default ShoeData;
